7ed7683c0b3f5c438bd406b728a608926992e3d3
nanfeng000 <166911117+nanfeng000@users.noreply.github.com>
2026-05-14 13:57:57 +0800

    DeepLabV3Plus

--- baseline/__init__.py
+++ baseline/__init__.py
@@ -1,3 +1,3 @@
-from .deeplabv3 import DeepLabV3ResNet50, build_deeplabv3_resnet50
+from .deeplabv3plus import DeepLabV3PlusResNet50, build_deeplabv3plus_resnet50
 
-__all__ = ["DeepLabV3ResNet50", "build_deeplabv3_resnet50"]
+__all__ = ["DeepLabV3PlusResNet50", "build_deeplabv3plus_resnet50"]
